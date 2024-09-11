7af7e5793b428ef5b3a92f7bcd85d43a93ebb52d
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-11 05:48:05 +0000

    needs styling/more recycling

--- app.rb
+++ app.rb
@@ -2,8 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+    erb(:home)
+end
+
+get("/rock") do
+  computer_move = rand(1..3)
+
+  @result = case computer_move
+           when 1
+             "They played rock. We tied."
+           when 2
+             "They played paper. We lost."
+           else
+             "They played scissors. We won."
+           end
+
+
+  erb(:rock)
+end
+
+get("/paper") do
+
+  computer_move = rand(1..3)
+
+  @result = case computer_move
+           when 1
+             "They played rock. We won."
+           when 2
+             "They played paper. We tied."
+           else
+             "They played scissors. We lost."
+           end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+
+  computer_move = rand(1..3)
+
+  @result = case computer_move
+           when 1
+             "They played rock. We lost."
+           when 2
+             "They played paper. We won."
+           else
+             "They played scissors. We tied."
+           end
+
+  erb(:scissors)
 end
